42b13f3e5fbb37c495b95c716910f220d526b88c
dinushchathurya <dinushchathurya21@gmail.com>
2022-02-15 22:33:08 +0530

    feat: Change validation message

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'mobile' => 'Invalid mobile number, please provide a valid Srilankan mobile number.'
+    'mobile' => 'Invalid telephone number, please provide a valid Srilankan telephone number.'
 ];
